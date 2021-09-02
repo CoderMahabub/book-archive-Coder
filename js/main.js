dcdedf859807aadc4e7c012810b47c12b09b63f5
CoderMahabub <codermahabub@gmail.com>
2021-09-02 19:22:23 +0600

    Finishing Touch

--- js/main.js
+++ js/main.js
@@ -8,15 +8,15 @@ const loadBooks = () => {
     const searchText = searchInput.value;
 
     if (searchText === "") {
-        errorDiv.classList.add('d-block', 'fw-bolder', 'text-danger');
-        errorDiv.innerText = "Search field cannot be empty.";
+        errorDiv.classList.add('d-block', 'fw-bolder', 'text-danger', 'mt-1');
+        errorDiv.innerText = "Search Field Cannot Be Empty!";
         return;
     }
     //Dynamic Search Text
-    const url = `http://openlibrary.org/search.json?q=${searchText}`
+    const url = `https://openlibrary.org/search.json?q=${searchText}`
     fetch(url)
         .then(res => res.json())
-        .then(data => displayBooks(data));
+        .then(data => displayBooks(data, data.docs));
 
     // Clear Search Text
     searchInput.value = '';
@@ -27,22 +27,21 @@ const loadBooks = () => {
 }
 
 //Display Books
-const displayBooks = data => {
+const displayBooks = (data, books) => {
 
     // Error Handing
     if (data.numFound === 0) {
-        errorDiv.classList.add('d-block', 'fw-bolder', 'text-danger');
+        errorDiv.classList.add('d-block', 'fw-bolder', 'text-danger', 'm-1');
         errorDiv.innerText = "NO Result Found ! Please Type Carefully";
     } else {
         errorDiv.innerText = "";
     }
-    const books = data.docs;
-    console.log(books);
 
     // Get Total Search Result
-    document.getElementById('total-items').innerText = books.length;
-
+    document.getElementById('total-items').innerText = parseInt(data.numFound);
+    // Repeating Part of Display
     books.forEach(book => {
+
         const div = document.createElement('div');
         div.classList.add('col-lg-6', 'card', 'mb-2');
         div.innerHTML = `
@@ -52,9 +51,10 @@ const displayBooks = data => {
         </div>
         <div class="col-md-8 d-flex align-items-center">
             <div class="card-body ">
-                <h5 class="card-title">${book.title}</h5>
-                <p class="card-text">by ${book.author_name}</p>
-                <p class="card-text">First Published In: <small class="text-muted">${book.first_publish_year}</small></p>
+                <h5 class="card-title fw-bolder">${book.title}</h5>
+                <p class="card-text">by <span class="text-success">${book.author_name}</span></p>
+                <p class="card-text">First Published In: <small class="text-success">${book.first_publish_year}</small></p>
+                <p class="card-text">Published By: <small class="text-muted">${book.publisher}</small></p>
             </div >
         </div >
         </div > `;
